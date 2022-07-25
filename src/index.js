fcb071dd98e4b399fb12f6c93abb77740967fa3e
JLivlos <mistlivlos@gmail.com>
2022-07-25 19:17:17 +0300

    make solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    const arr = n.toString().split('');
+    if (n < 0) {
+        return +arr.splice(1, arr.length - 1).reverse().join('');
+    };
+    return +arr.reverse().join('');
 }
